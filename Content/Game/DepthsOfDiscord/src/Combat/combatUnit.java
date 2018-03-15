5816d4add37f7bebce9a42fd2cae7746fd4abdd8
Richard <rgantt@udel.edu>
2018-03-15 03:29:37 -0400

    started game development by creating combatUnit interface and enemy class. subject to change

---
+++ Content/Game/DepthsOfDiscord/src/Combat/combatUnit.java
@@ -0,0 +1,31 @@
+package Combat;
+
+public interface combatUnit {
+	
+	public void takeDamage(){
+		
+	}
+	
+	
+	public int meleeAttack(){
+		
+	}
+	
+	public void incap(){
+		
+	}
+	
+	public void useItem(){
+		
+	}
+	
+	public void heal(){
+		
+	}
+	
+	public String appearance(){
+		
+	}
+	
+	
+}
