4533824ceae8563fda7ecebed0c3bb441f4f0fd0
Richard <rgantt13@gmail.com>
2024-04-29 10:23:32 -0400

    Removed node_modules from version control and added a .gitignore file to ensure that it does not get returned in future commits.

--- Content/Bot/index.js
+++ Content/Bot/index.js
@@ -1,18 +1,22 @@
 // JavaScript source code
+//import botconfig from "../../../BotConfig/botconfig.json" assert { type: "json" }
 const botconfig = require("../../../BotConfig/botconfig.json");
 const Discord = require("discord.js");
+//import {Client, Collection, GatewayIntentBits } from "discord.js"
 
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client({intents : [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.MessageContent, Discord.GatewayIntentBits.GuildMessages]});
+//const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", async () => {
-	bot.user.setUsername("TestBot");
+bot.on("ready", () => {
+	bot.user.setUsername("NewBot");
 	console.log(`${bot.user.username} is online!`);
 	
-	bot.user.setGame("Fuck you~! :)");
+	//bot.user.setActivity("Fuck you~! :)");
 });
 
+bot.on(Discord.Events.MessageCreate, message => {
+	console.log(message);
 
-bot.on("message", async message => {
 	if(message.author.bot) return;
 	if(message.channel.type === "dm") return;
 
@@ -22,7 +26,38 @@ bot.on("message", async message => {
 	let args = messageArray.slice(1);
 
 
-	//!say Hello
+	if(message.content == "hello"){
+		console.log("yo")
+		message.reply("bitch.");
+	}
+
+	if(cmd === `${prefix}hello`){
+		return message.channel.send("Hello");
+	}
+
+	if(cmd === `${prefix}progress`){
+		return message.channel.send("So far I have set up the ability to say 'Hello!'");
+	}
+
+})
+
+
+bot.on("message", message => {
+	console.log(message);
+
+	if(message.author.bot) return;
+	if(message.channel.type === "dm") return;
+
+	let prefix = botconfig.prefix;
+	let messageArray = message.content.split(" ");
+	let cmd = messageArray[0];
+	let args = messageArray.slice(1);
+
+
+	if(message.content == "hello"){
+		message.reply("bitch.");
+	}
+
 	if(cmd === `${prefix}hello`){
 		return message.channel.send("Hello");
 	}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-This is the readme for this project
+###RUN INSTRUCTIONS
+
+To run this project install the packages using the following command:
+
+npm i
+
+When packages are successfully installed, you can run the discord bot with this command:
+
+npm start
+
+###Resources
+
+Troubleshooting access/permissions:
+https://stackoverflow.com/questions/73036854/message-content-doesnt-have-any-value-in-discord-js
